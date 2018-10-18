cf965a0914fff19bf299fd7c054e67032c04f246
P-Vries <43247243+P-Vries@users.noreply.github.com>
2018-10-18 13:06:13 +0200

    Add files via upload

--- BterKaasEieren/Program.cs
+++ BterKaasEieren/Program.cs
@@ -16,7 +16,7 @@ namespace BterKaasEieren
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmBoterkaasUI());
         }
     }
 }
---
+++ BterKaasEieren/frmBoterkaasUI.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace BterKaasEieren
+{
+    partial class frmBoterkaasUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlayer1 = new System.Windows.Forms.Button();
+            this.btnPlayer2 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblBO = new System.Windows.Forms.Label();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // btnPlayer1
+            // 
+            this.btnPlayer1.BackColor = System.Drawing.Color.Gray;
+            this.btnPlayer1.Location = new System.Drawing.Point(12, 12);
+            this.btnPlayer1.Name = "btnPlayer1";
+            this.btnPlayer1.Size = new System.Drawing.Size(75, 23);
+            this.btnPlayer1.TabIndex = 0;
+            this.btnPlayer1.Text = "Speler 1";
+            this.btnPlayer1.UseVisualStyleBackColor = false;
+            this.btnPlayer1.Click += new System.EventHandler(this.btnPlayer1_Click);
+            // 
+            // btnPlayer2
+            // 
+            this.btnPlayer2.BackColor = System.Drawing.Color.Gray;
+            this.btnPlayer2.Location = new System.Drawing.Point(174, 12);
+            this.btnPlayer2.Name = "btnPlayer2";
+            this.btnPlayer2.Size = new System.Drawing.Size(75, 23);
+            this.btnPlayer2.TabIndex = 1;
+            this.btnPlayer2.Text = "Speler 2";
+            this.btnPlayer2.UseVisualStyleBackColor = false;
+            this.btnPlayer2.Click += new System.EventHandler(this.btnPlayer2_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.Gainsboro;
+            this.btn2.Location = new System.Drawing.Point(93, 41);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 75);
+            this.btn2.TabIndex = 2;
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.Gainsboro;
+            this.btn1.Location = new System.Drawing.Point(12, 41);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 75);
+            this.btn1.TabIndex = 3;
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.Color.Gainsboro;
+            this.btn3.Location = new System.Drawing.Point(174, 41);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 75);
+            this.btn3.TabIndex = 4;
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.Gainsboro;
+            this.btn4.Location = new System.Drawing.Point(12, 122);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 75);
+            this.btn4.TabIndex = 5;
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.Color.Gainsboro;
+            this.btn5.Location = new System.Drawing.Point(93, 122);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(75, 75);
+            this.btn5.TabIndex = 6;
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.Color.Gainsboro;
+            this.btn6.Location = new System.Drawing.Point(174, 122);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(75, 75);
+            this.btn6.TabIndex = 7;
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.Color.Gainsboro;
+            this.btn7.Location = new System.Drawing.Point(12, 203);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(75, 75);
+            this.btn7.TabIndex = 8;
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.Color.Gainsboro;
+            this.btn8.Location = new System.Drawing.Point(93, 203);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(75, 75);
+            this.btn8.TabIndex = 9;
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.Color.Gainsboro;
+            this.btn9.Location = new System.Drawing.Point(174, 203);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(75, 75);
+            this.btn9.TabIndex = 10;
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 305);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Beurt:";
+            // 
+            // lblBO
+            // 
+            this.lblBO.AutoSize = true;
+            this.lblBO.Location = new System.Drawing.Point(53, 305);
+            this.lblBO.Name = "lblBO";
+            this.lblBO.Size = new System.Drawing.Size(13, 13);
+            this.lblBO.TabIndex = 12;
+            this.lblBO.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 343);
+            this.Controls.Add(this.lblBO);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btnPlayer2);
+            this.Controls.Add(this.btnPlayer1);
+            this.Name = "Form1";
+            this.Text = "Boter-Kaas-Eieren";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlayer1;
+        private System.Windows.Forms.Button btnPlayer2;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblBO;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
